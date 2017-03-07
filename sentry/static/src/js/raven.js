c41e86befd83a1b396e7537d20473488fa83510b
Naglis Jonaitis <naglis@versada.eu>
2017-03-07 11:03:48 +0200

    sentry/raven-js: adds the missing semicolons which break Odoo client after assets are minified

--- sentry/static/src/js/raven.js
+++ sentry/static/src/js/raven.js
@@ -28,12 +28,12 @@ function serializer(replacer, cycleReplacer) {
 
   return function(key, value) {
     if (stack.length > 0) {
-      var thisPos = stack.indexOf(this)
-      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
-      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
-      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
+      var thisPos = stack.indexOf(this);
+      ~thisPos ? stack.splice(thisPos + 1) : stack.push(this);
+      ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key);
+      if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value);
     }
-    else stack.push(value)
+    else stack.push(value);
 
     return replacer == null ? value : replacer.call(this, key, value)
   }
