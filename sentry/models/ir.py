3b38f8355e0c62accb3ed06737fcd3d00185bdb3
Naglis Jonaitis <naglis@versada.eu>
2017-03-07 11:07:17 +0200

    sentry: some refactoring

--- sentry/models/ir.py
+++ sentry/models/ir.py
@@ -9,6 +9,12 @@ from odoo.tools import config as odoo_config
 
 
 def dsn_remove_secret_key(dsn):
+    '''
+    Returns the DSN with the secret key part removed.
+
+    This is needed for client-side (publicly visible) Sentry integrations,
+    eg. with raven-js.
+    '''
     result = urlparse.urlsplit(dsn)
     return result._replace(
         netloc='{0.username}@{0.hostname}'.format(result),
@@ -22,11 +28,13 @@ class IrHttp(models.AbstractModel):
         res = super(IrHttp, self).webclient_rendering_context()
         user = http.request.env.user
         dsn = odoo_config.get('sentry_dsn')
-        res['sentry_data'] = {
-            'dsn': dsn_remove_secret_key(dsn) if dsn else '',
-            'user': {
-                'name': user.display_name,
-                'email': user.email,
-            },
-        }
+        res.update({
+            'sentry_data': {
+                'dsn': dsn_remove_secret_key(dsn) if dsn else '',
+                'user': {
+                    'name': user.display_name,
+                    'email': user.email,
+                },
+            }
+        })
         return res
