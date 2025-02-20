289e6e88f2dbac981765fa695b488a717cd29c42
Andrius Laukavičius <anlaukavic@gmail.com>
2025-02-20 17:07:23 +0200

    [REF] base_multi_image: pre-commit

--- base_multi_image/models/image.py
+++ base_multi_image/models/image.py
@@ -76,9 +76,7 @@ class Image(models.Model):
     def _compute_show_technical(self):
         """Know if you need to show the technical fields."""
         self.show_technical = all(
-            "default_owner_{}s".format(f) not in self.env.context for f in (
-                "id", "model"
-            )
+            f"default_owner_{f}s" not in self.env.context for f in ("id", "model")
         )
 
     @api.onchange("load_from")
